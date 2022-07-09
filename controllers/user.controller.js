73e654c704d74d9f6f8b3ec4d5a632d285a60e05
vivie16 <virginiedevaine@gmail.com>
2022-07-09 15:38:23 +0200

    probleme following

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,7 +4,7 @@ const objectID = require('mongoose').Types.ObjectId;
 module.exports.getAllUsers = async(req, res) => {
     const users = await userModel.find().select('-password');
     res.status(200).json(users);
-}
+};
 
 module.exports.userInfo = (req, res) =>{
     if (!objectID.isValid(req.params.id))
@@ -35,7 +35,7 @@ module.exports.updateUser = async (req, res) => {
             }
         )
     } catch (err) {
-        return res.status(500).send({message : err});
+        return res.status(500).json({message : err});
     }
 };
 module.exports.deleteUser = async (req, res) => {
@@ -46,11 +46,11 @@ module.exports.deleteUser = async (req, res) => {
         await userModel.remove({_id: req.params.id});
         res.status(200).json({ message: "Successfully deleted."})
     } catch (err) {
-        return res.status(500).send({message : err});
+        return res.status(500).json({message : err});
     }
 };
 module.exports.follow = async (req, res) => {
-    if (!objectID.isValid(req.params.id))
+    if (!objectID.isValid(req.params.id) || !objectID.isValid(req.body.idToFollow))
         return res.status(400).send('Id unknown : ' + req.params.id)
     
     try { 
@@ -61,7 +61,7 @@ module.exports.follow = async (req, res) => {
             { new: true, upsert: true},
             (err, docs) => {
                 if (!err) res.status(201).json(docs);
-                else return res.status(400).json(err);
+                else return res.status(400).jsos(err);
             },
         );
         // add to following list
@@ -70,21 +70,38 @@ module.exports.follow = async (req, res) => {
             { $addToSet: { followers: req.params.id}},
             { new: true, upsert: true},
             (err, docs) => {
-                if(err) return res.status(400).json(err);
+                if(err) return res.status(400).jsos(err);
             },
         )
-
     } catch (err) {
-        return res.status(500).send({message : err});
+        return res.status(500).json({message : err});
     }
 };
 module.exports.unfollow = async (req, res) => {
-    if (!objectID.isValid(req.params.id))
-        return res.status(400).send('Id unknown : ' + req.params.id)
-    
-    try { 
+    if (!objectID.isValid(req.params.id) || !objectID.isValid(req.body.idToUnfollow))
+    return res.status(400).send('Id unknown : ' + req.params.id)
 
+    try { 
+        //add to the unfollower list
+        await userModel.findByIdAndUpdate(
+            req.params.id,
+            { $pull: { following: req.body.idToUnfollow}},
+            { new: true, upsert: true},
+            (err, docs) => {
+                if (!err) res.status(201).json(docs);
+                else return res.status(400).jsos(err);
+            },
+        );
+        // add to unfollowing list
+        await userModel.findByIdAndUpdate (
+            req.body.idToUnfollow,
+            { $pull: { followers: req.params.id}},
+            { new: true, upsert: true},
+            (err, docs) => {
+                if(err) return res.status(400).jsos(err);
+            },
+        )
     } catch (err) {
-        return res.status(500).send({message : err});
+        return res.status(500).json({message : err});
     }
 };
